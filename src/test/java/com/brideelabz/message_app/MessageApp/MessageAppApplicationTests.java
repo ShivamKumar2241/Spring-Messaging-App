72d3f6dff7a4570509157d289c074688eeb7f05e
shivamrajput18 <shivamsingh80094@gmail.com>
2025-05-20 15:47:29 +0530

    [shivam] Add : MessageApp Project

---
+++ src/test/java/com/brideelabz/message_app/MessageApp/MessageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brideelabz.message_app.MessageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
